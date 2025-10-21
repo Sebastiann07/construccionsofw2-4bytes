1b959f08bee2dacd8bf7e017b53b7d6f993ec681
Miguelquintero22 <migueltdea@gmail.com>
2025-10-20 19:35:28 -0500

    ports

---
+++ trabajo/src/main/java/app/domain/services/CreatePatient.java
@@ -0,0 +1,29 @@
+package app.domain.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.domain.ports.PatientPort;
+import app.domain.model.User;
+
+@Service
+public class CreatePatient {
+	
+	@Autowired
+	private PatientPort patientPort;
+
+	public CreatePatient(PatientPort patientPort) {
+		this.patientPort = patientPort;
+	}
+
+    public void createPerson(User patient) throws Exception {
+		if (patientPort.findById(patient.getId()) != null) { 
+			throw new Exception("Ya existe una persona con este documento");
+		}
+		if (patientPort.findByFullName(patient.getFullName()) != null) {
+			throw new Exception("Ya existe una persona con este nombre");
+		}
+		patientPort.save(patient);
+		}
+
+}
